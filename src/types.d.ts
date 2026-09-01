ee53a35264de9a10370672a89c72d68db9044837
Raissa Mendonca <rmarques@kent.edu>
2026-09-01 16:32:39 -0400

    Decrease whitespace between page title and element

--- src/types.d.ts
+++ src/types.d.ts
@@ -194,6 +194,7 @@ export interface Hero extends Omit<Headline, 'classes'>, Omit<Widget, 'isDark' |
   content?: string;
   actions?: string | CallToAction[];
   image?: string | unknown;
+  compact?: boolean;
 }
 
 export interface Team extends Omit<Headline, 'classes'>, Widget {
